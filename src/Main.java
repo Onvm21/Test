95ecf1959981596a2ce09fd68ca319c94001ac9b
Onvm2007 <nicks2026@student.cis.edu.hk>
2023-08-30 19:49:34 +0800

    added new line

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("this stupid thing is so annoying");
         System.out.println("Hello, World");
+        System.out.println("this stupid thing is so annoying");
     }
 }
